c38ee6231a62f427115c29af2ccc300a915b8d17
Iamneo <techadmin@iamneo.ai>
2023-12-01 10:36:55 +0000

    Submit-project-save-commit-12/1/2023--4:06:54-PM

--- springapp/src/main/java/com/example/springapp/Calculator.java
+++ springapp/src/main/java/com/example/springapp/Calculator.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
     System.out.println("welcome");
     System.out.println("sai");
     System.out.println("qwerty");
+    System.out.println("werlc");
 }
 
 }
